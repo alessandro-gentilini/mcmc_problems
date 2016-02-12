0284d910c76b87140a98a773b9a2207403bad1d2
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2016-02-12 20:41:02 +0100

    It does not seem to me that the result is correct...

--- Problem_2.py
+++ Problem_2.py
@@ -4,22 +4,36 @@ import random
 def gaussian_density(x,mu,sigma):
     return exp(-(x-mu)**2/(2*sigma**2))/(sigma*sqrt(2*pi))
 
-def mcmc(f,q,theta_k):
+def next_sample(f,q,theta_k):
     theta_prime = q(0,1)
 
     r = random.random()
 
     theta_k_plus_one = theta_k
 
-    if (f(theta_prime,2,sqrt(2))/f(theta_k,2,sqrt(2))>r):
+    if ((f(theta_prime,2,sqrt(2))/f(theta_k,2,sqrt(2)))>r):
         theta_k_plus_one = theta_prime
 
     return theta_k_plus_one
 
-thetas = [] # is this the 'chain'?!
+
+random.seed(3.14)
+thetas = []
 theta = 0
-for i in range(10**4):
-    theta = mcmc(gaussian_density,random.gauss,theta)
+thetas.append(theta)
+for i in range(10**5):
+    theta = next_sample(gaussian_density,random.gauss,theta)
     thetas.append(theta)
 
-print thetas    
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.hist(thetas,normed=True)
+x=np.linspace(min(thetas),max(thetas))
+
+y=[]
+for xx in x:
+    y.append(gaussian_density(xx,2,sqrt(2)))
+
+plt.plot(x,y)
+plt.show()
